1afa17732e629f7a99246a0870acbee3eda34d45
Alveena Sohail <alveena.sohail21@gmail.com>
2018-08-08 17:52:36 +0500

    attachment button added

--- src/app/chat/services/conversations.service.ts
+++ src/app/chat/services/conversations.service.ts
@@ -12,7 +12,7 @@ export class ConversationsService {
     { id: 2, username: 'Muhammad Nauman', description: 'muhammadnauman@gmail.com', avatar: 'http://emilcarlsson.se/assets/mikeross.png' },
     { id: 3, username: 'Ahmed Waqas Nasir', description: 'ahmedwaqasnasir@gmail.com', avatar: 'http://emilcarlsson.se/assets/mikeross.png' },
     { id: 4, username: 'Taha Qadri', description: 'tahaqadri@gmail.com', avatar: 'http://emilcarlsson.se/assets/mikeross.png' },
-    { id: 5, username: 'Asif', description: 'asf@gmail.com', avatar: 'http://emilcarlsson.se/assets/mikeross.png' },
+    { id: 5, username: 'Muhammad Asif', description: 'asf@gmail.com', avatar: 'http://emilcarlsson.se/assets/mikeross.png' },
   ];
 
   getConversations = () => {
