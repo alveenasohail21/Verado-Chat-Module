f276af01aad4093926444db507bf57cd0ca894b0
Alveena Sohail <alveena.sohail21@gmail.com>
2018-08-08 15:04:18 +0500

    solve bug

--- src/app/chat/inbox/inbox.component.ts
+++ src/app/chat/inbox/inbox.component.ts
@@ -1,3 +1,4 @@
+import { ConversationsService } from './../services/conversations.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -11,6 +12,7 @@ export class InboxComponent implements OnInit {
   conversationId: Number;
 
   constructor(public router: Router, private route: ActivatedRoute) {
+ this.conversationsService.startChat = false;
     this.route.params.subscribe((params) => {
       this.conversationId = params.id;
     });
